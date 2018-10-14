0b056e4bca0f232c6de64c3839b2ee9a816c1cc8
Kamil Inglot <outglot@gmail.com>
2018-10-14 18:51:32 +0200

    CopyBoard class

--- src/MiniMax.java
+++ src/MiniMax.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 
 public class MiniMax {
 	
@@ -13,9 +14,9 @@ public class MiniMax {
     }
  
     public void constructTree(int noOfBones) {
-        tree = new SearchTree(null);
+        //tree = new SearchTree(null);
         TreeNode root = new TreeNode(board, currentP, null);
-        tree.setRoot(root);
+        //tree.setRoot(root);
         constructTree(root);
     }
  
@@ -32,8 +33,25 @@ public class MiniMax {
         });
     }
     
-    
+    //Player1 is 1, Player2 is 2, Arrow is 3, empty spots are 0
     private int[][] copyBoard(GameTile[][] b) {
+    	int board[][] = new int[b.length][b[0].length];
+    	for(int x = 0; x < b.length; x++) {
+    		for (int y = 0; y < b[0].length; y++) {
+    			if (b[x][y].hasPiece) {
+    				if(b[x][y].piece.color == Color.WHITE){
+    					board[x][y] = 1;
+    				}
+    				else if (b[x][y].piece.color == Color.BLACK) {
+    					board[x][y] = 2;
+    				}
+    			
+    				else (b[x][y].shot) {
+    				board[x][y] = 3;
+    				}
+    			}
+    		}
+    	}
     	
     }
     
