28124d8b23de1890596baff57ab8bc45988e1dbb
Kamil Inglot <outglot@gmail.com>
2018-10-15 19:51:28 +0200

    Method for check if game finished

--- src/MiniMax.java
+++ src/MiniMax.java
@@ -8,6 +8,8 @@ public class MiniMax {
     private GameTile[][] board;
     private String currentP;
     private TreeNode root ;
+    private boolean WhiteWon = false;
+    private boolean BlackWon = false;
     int[][] boardCopy;
     int depthLimit ;
     
@@ -48,6 +50,9 @@ public class MiniMax {
     	
     	if(limit == 0){
     		return ;
+    	}
+    	if (isFinished(boardCopy, node)) {
+    		return;
     	}
     	
     	if(node.getPlayer().equals("White")){
@@ -208,7 +213,32 @@ public class MiniMax {
     	
     }
     
-    
-    
+    //Check for moves possible moves for given player, if there are any, game may continue, 
+    //if not, method isFinished is true
+    private boolean isFinished(int[][] b, TreeNode node) {
+    	if(node.getPlayer().equals("White")){
+    		for(int i =0; i < b.length; i++){
+    			for(int j= 0; j < b[0].length; j++){
+    				if(b[i][j] == 1){
+    					ArrayList<Point> possibleM = checkForLegalMoves(i,j, b);
+    					if (possibleM.size() > 0) return false;
+    				}
+    			}
+    		}
+    	}
+    	else {
+    		for(int i =0; i < b.length; i++){
+    			for(int j= 0; j < b[0].length; j++){
+    				if(b[i][j] == 2){
+    					ArrayList<Point> possibleM = checkForLegalMoves(i,j, b);
+    					if (possibleM.size() > 0) return false;
+    				}
+    			}
+    		}
+    	}
+    	if (node.getPlayer().equals("White")) BlackWon = true;
+    	if (node.getPlayer().equals("Black")) WhiteWon = true;
+    	return true;
+    }
     
 }
